705d767ab85776b1c5d14d2ea2723a7c3a303272
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2022-08-25 14:19:50 -0600

    chess board set up

--- src/com/noah/chess/Main.java
+++ src/com/noah/chess/Main.java
@@ -2,21 +2,68 @@ package com.noah.chess;
 
 import javafx.stage.*;
 import javafx.application.Application;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 
 public class Main extends Application {
+	
+	private char turn = 'W';
+	Cell[][] cell = new Cell[8][8];
+	
+	GridPane pane;
+	
 	@Override
 	public void start(Stage stage) {
-		Pane p = new Pane();
+		pane = new GridPane();
+		pane.setCursor(Cursor.OPEN_HAND);
 		
-		Scene s = new Scene(p, 700, 500);
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				pane.add(cell[i][j] = new Cell(), i, j);
+				
+				if(i % 2 == 0) {
+					if(j % 2 == 0) {
+						cell[i][j].setStyle("-fx-background-color: white;");
+					} else {
+						cell[i][j].setStyle("-fx-background-color: black;");
+					}
+				} else {
+					if(j % 2 == 0) {
+						cell[i][j].setStyle("-fx-background-color: black;");
+					} else {
+						cell[i][j].setStyle("-fx-background-color: white;");
+					}
+				}
+			}
+		}
+		
+		Scene s = new Scene(pane, 500, 500);
 		
 		stage.setTitle("Chess");
 		stage.setResizable(false);
 		stage.setScene(s);
 		stage.show();
 	}
+	
+	public class Cell extends Pane {
+		private String token = " ";
+		private Text t;
+		
+		public Cell() {
+			this.setPrefSize(2000, 2000);
+		}
+		
+		public String getToken() {
+			return token;
+		}
+		public void setToken(String token) {
+			this.token = token;
+		}
+
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}

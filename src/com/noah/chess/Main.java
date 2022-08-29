95f17a58a3b04742c25bd2ccfb2200e5c3cd56d6
404kevnotfound <44481502+TweakyTurnip875@users.noreply.github.com>
2022-08-29 14:27:34 -0600

    set pawn positions

--- src/com/noah/chess/Main.java
+++ src/com/noah/chess/Main.java
@@ -1,13 +1,33 @@
 package com.noah.chess;
 
 import javafx.stage.*;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 public class Main extends Application {
+	
+	String pawn_w = "images/white/pawn_w.png",
+		   rook_w = "images/white/rook_w.png",
+		   knight_w = "images/white/knight_w.png",
+		   bishop_w = "images/white/bishop_w.png",
+		   queen_w = "images/white/qween_w.png",
+		   king_w = "images/white/king_w.png";
+	
+	String pawn_b = "images/black/pawn_b.png",
+		   rook_b = "images/black/rook_b.png",
+		   knight_b = "images/black/knight_b.png",
+		   bishop_b = "images/black/bishop_b.png",
+		   queen_b = "images/black/qween_b.png",
+		   king_b = "images/black/king_b.png";
 	
 	private char turn = 'W';
 	Cell[][] cell = new Cell[8][8];
@@ -15,7 +35,7 @@ public class Main extends Application {
 	GridPane pane;
 	
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage stage) throws FileNotFoundException {
 		pane = new GridPane();
 		pane.setCursor(Cursor.OPEN_HAND);
 		
@@ -36,6 +56,35 @@ public class Main extends Application {
 						cell[i][j].setStyle("-fx-background-color: white;");
 					}
 				}
+				
+				if(j == 1) {
+					cell[i][j].setToken(pawn_b);
+				} else if((i == 0 && j == 0) || (i == 7 && j == 0)) {
+					cell[i][j].setToken(rook_b);
+				} else if((i == 1 && j == 0) || (i == 6 && j == 0)) {
+					cell[i][j].setToken(knight_b);
+				} else if((i == 2 && j == 0) || (i == 5 && j == 0)) {
+					cell[i][j].setToken(bishop_b);
+				} else if(i == 3 && j == 0) {
+					cell[i][j].setToken(queen_b);
+				} else if(i == 4 && j == 0) {
+					cell[i][j].setToken(king_b);
+				}
+				
+				
+				if(j == 6) {
+					cell[i][j].setToken(pawn_w);
+				} else if((i == 0 && j == 7) || (i == 7 && j == 7)) {
+					cell[i][j].setToken(rook_w);
+				} else if((i == 1 && j == 7) || (i == 6 && j == 7)) {
+					cell[i][j].setToken(knight_w);
+				} else if((i == 2 && j == 7) || (i == 5 && j == 7)) {
+					cell[i][j].setToken(bishop_w);
+				} else if(i == 3 && j == 7) {
+					cell[i][j].setToken(queen_w);
+				} else if(i == 4 && j == 7) {
+					cell[i][j].setToken(king_w);
+				}
 			}
 		}
 		
@@ -49,17 +98,41 @@ public class Main extends Application {
 	
 	public class Cell extends Pane {
 		private String token = " ";
-		private Text t;
+		private Image t;
+		private ImageView tV;
 		
 		public Cell() {
 			this.setPrefSize(2000, 2000);
 		}
 		
+		public void handleToken(String token) throws FileNotFoundException {
+			tV = new ImageView();
+			tV.setFitHeight(50);
+			tV.setFitWidth(30);
+			tV.setTranslateX(15);
+			tV.setTranslateY(5);
+			
+			if(this.token.equals(pawn_w)) {
+				t = new Image("File:" + pawn_w);
+				
+				tV.setImage(t);
+				
+				getChildren().add(tV);
+			} else if(this.token.equals(pawn_b)) {
+				t = new Image("File:" + pawn_b);
+				
+				tV.setImage(t);
+				
+				getChildren().add(tV);
+			}
+		}
 		public String getToken() {
 			return token;
 		}
-		public void setToken(String token) {
+		public void setToken(String token) throws FileNotFoundException {
 			this.token = token;
+			
+			handleToken(token);
 		}
 
 	}
